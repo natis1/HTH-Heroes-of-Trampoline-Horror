30ad4f939b230d0a912bfce05ce012e6211eb968
BananaSky <lucassaldyt@yahoo.com>
2016-02-08 13:34:51 -0700

    Update of python module - simplified to using an external library

--- image.py
+++ image.py
@@ -1,21 +1,19 @@
+from noise import pnoise2, snoise2
 from PIL    import Image
-import Perlin
 
-img_size = int(input("Enter the size of the image:"))
+img_size = 256
+#img_size = int(input("Enter the size of the image:"))
 
-def gen_Pmap(size):
-    noise = Perlin.gen_Perlin(size)
-    data = []
-    for node in noise:
-        data.append((node, node, node, 255))
-    return data
+octaves = 4
+freq = 16.0 * octaves
+data = []
+for y in range(img_size):
+    for x in range(img_size):
+        v = int(snoise2(x / freq, y / freq, octaves) * 127.0 + 128.0)
+        data.append((v, v, v, 255))
+
+print(data)
 
 img = Image.new('RGBA', (img_size, img_size))
-img.putdata(gen_Pmap(img_size))
+img.putdata(data)
 img.save("image.png")
-
-# Here is a cloud nine project so you can see how it works (interpreter included)
-# https://ide.c9.io/bananasky/image_creation 
-
-# Here is an example of noise generated with this method: 
-# http://i.imgur.com/4qOMeTs.png
