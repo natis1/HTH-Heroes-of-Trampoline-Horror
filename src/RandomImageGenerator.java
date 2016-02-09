6350bf78efaef8ffaeafe1c65c022490faecd004
voidling <eli.stonium@gmail.com>
2016-02-09 13:04:58 -0700

    Switched Image generation to Simplex
    This is a completely open n dimensional generator. Currently using 3D with
    24 bits of height.

--- src/RandomImageGenerator.java
+++ src/RandomImageGenerator.java
@@ -3,22 +3,35 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-/**
- * Created by scc on 2/8/2016.
- */
+
+
 public class RandomImageGenerator {
+
+    private static int FEATURE_SIZE = 16;
 
 
     public void RandomImageGenerator(String saveLocation, int height, int width) {
 
 
 
-        BufferedImage imageToWrite = generateBufferedImage(height, width);
+        OpenSimplexNoise noise = new OpenSimplexNoise();
+        BufferedImage image = new BufferedImage(height, width, BufferedImage.TYPE_INT_RGB);
+
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                double value = noise.eval(x / FEATURE_SIZE, y / FEATURE_SIZE, 0.0);
+                int rgb = 0x010101 * (int)((value + 1) * 127.5);
+                image.setRGB(x, y, rgb);
+            }
+        }
 
 
         File imageWriteLocation = new File(saveLocation);
         try {
-            ImageIO.write(imageToWrite, "png", imageWriteLocation);
+            ImageIO.write(image, "png", imageWriteLocation);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -33,26 +46,23 @@ public class RandomImageGenerator {
 
 
     private BufferedImage generateBufferedImage(int height, int width) {
-        BufferedImage img = null;
 
-        for (int y = 0; y < height; y++){
-            for (int x = 0; x < width; x++){
-                int a = 255;
-                int r = (int) (Math.random() * 256);
-                int g = (int) (Math.random() * 256);
-                int b = (int) (Math.random() * 256);
+        OpenSimplexNoise noise = new OpenSimplexNoise();
+        BufferedImage image = new BufferedImage(height, width, BufferedImage.TYPE_INT_RGB);
 
 
-                int pixel = (a << 24) | (r << 16) | (g << 8) | b; //a * 24, r * 16, g * 8, b. BITMAP FORMAT
-
-
-                img.setRGB(x, y, pixel);
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                double value = noise.eval(x / FEATURE_SIZE, y / FEATURE_SIZE, 0.0);
+                int rgb = 0x010101 * (int)((value + 1) * 127.5);
+                image.setRGB(x, y, rgb);
             }
         }
 
 
-
-        return img;
+        return image;
     }
 
 
