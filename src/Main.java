6184d35dd316df48107e48f96eb0f705b3d39120
voidling <eli.stonium@gmail.com>
2016-02-26 10:29:21 -0700

    Added a λ to main because I like half life

--- src/Main.java
+++ src/Main.java
@@ -28,24 +28,22 @@ public class Main
     public static void main(String[] args)
     {
         EventQueue.invokeLater(
-                new Runnable() {
-                        @Override
-                        public void run()
-                        {
+                () -> {
 
-                            //BOW TO THE VOID OR BE CONSUMED BY IT
+                    //BOW TO THE VOID OR BE CONSUMED BY IT
 
-                            //Honestly I am so glad nobody has noticed this or the window loader name.
+                    //Honestly I am so glad nobody has noticed this or the window loader name.
 
-                            //PS since its an elf a Malzahar quote is kinda unfitting, maybe something like:
-                            //"A solid giggle should do the trick!"
+                    //PS since its an elf a Malzahar quote is kinda unfitting, maybe something like:
+                    //"A solid giggle should do the trick!"
 
-                            //Literally don't even need to name a windowloader here I don't think. But whatevs
+                    //Literally don't even need to name a windowloader here I don't think. But whatevs
 
-                            //hmmm..
-                            WindowLoader ElvenMasterRulesOverPunyHumans = new WindowLoader();
+                    //hmmm..
 
-                        }
+                    //Clearly I am going insane.
+                    WindowLoader ElvenMasterRulesOverPunyHumans = new WindowLoader();
+
                 }
         );
     }
