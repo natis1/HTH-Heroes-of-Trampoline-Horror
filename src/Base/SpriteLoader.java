7caaa88e4091d86ddcf7ecb18fb9b7688f5e5e6b
voidling <eli.stonium@gmail.com>
2016-03-08 11:13:33 -0700

    Removed some burocratic code, TBD what I do with it

--- src/Base/SpriteLoader.java
+++ src/Base/SpriteLoader.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+
 public class SpriteLoader {
 
     private static HashMap<String, BufferedImage> images;
@@ -18,23 +19,20 @@ public class SpriteLoader {
         backgroundKeys = new ArrayList<>();
         String dir  = System.getProperty("user.dir") + "/src/Base/Resources/";
         loadDir(dir);
-        backgroundKeys.add("grass"); //Don't need to do this manually, but for now, it will be alright
-        backgroundKeys.add("sidewalk");
+        backgroundKeys.add("overworldGrass"); //Don't need to do this manually, but for now, it will be alright
+        backgroundKeys.add("overworldSidewalk");
     }
 
-    public BufferedImage unsafeGetBackgroundImage (int index)
-    {
+    public BufferedImage unsafeGetBackgroundImage (int index) {
         index %= backgroundKeys.size();
         return images.get(backgroundKeys.get(index));
     }
 
-    public BufferedImage returnImageFromSet (String index)
-    {
+    public BufferedImage returnImageFromSet (String index) {
         return images.get(index);
     }
 
-    public void loadDir(String directory)
-    {
+    public void loadDir(String directory) {
         File dir = new File(directory);
         if(dir.isDirectory())
         {
@@ -52,7 +50,7 @@ public class SpriteLoader {
         }
     }
 
-    public BufferedImage loadImage(String image_file){
+    public BufferedImage loadImage(String image_file) {
         BufferedImage img;
         try
         {
