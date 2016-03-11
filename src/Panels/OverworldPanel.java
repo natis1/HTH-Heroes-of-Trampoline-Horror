9d28aea18260362630e6282895ca820d6d551b9a
voidling <eli.stonium@gmail.com>
2016-03-11 11:09:17 -0700

    Moved location of files

--- src/Panels/OverworldPanel.java
+++ src/Panels/OverworldPanel.java
@@ -75,31 +75,6 @@ public class OverworldPanel extends BasePanel implements ActionListener, MouseLi
     }
 
 
-    private void addImageWithAlphaComposite(BufferedImage buff1, BufferedImage buff2, float opaque, int x, int y) {
-        Graphics2D g2d = buff1.createGraphics();
-        g2d.setComposite(
-                AlphaComposite.getInstance(AlphaComposite.SRC_OVER, opaque));
-        g2d.drawImage(buff2, x, y, null);
-        g2d.dispose();
-    }
-
-    private BufferedImage addImageUsingSetRGB(BufferedImage src, BufferedImage dst, int dx, int dy) {
-        for (int x = 0; x < src.getWidth(); x++) {
-            for (int y = 0; y < src.getHeight(); y++) {
-                dst.setRGB( dx + x, dy + y, src.getRGB(x,y) );
-            }
-        }
-        return dst;
-    }
-
-    private BufferedImage copyColoredPixelsIntoBufferedImage(BufferedImage dst, int dx, int dy, int sizex, int sizey, Color color) {
-        Graphics2D g = dst.createGraphics();
-        g.setColor(color);
-        g.fillRect(dx, dy, sizex, sizey);
-        g.dispose();
-        return dst;
-    }
-
     private void reloadMapSprites() {
         //TODO check if chunk changed
 
