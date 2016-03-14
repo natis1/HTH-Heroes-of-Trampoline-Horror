f91c818ae8d3b6ce34cddc5e3f5264b293806f73
voidling <eli.stonium@gmail.com>
2016-03-14 13:18:10 -0600

    Added some space. IDK I get really OCD about this

--- src/Base/Weapon.java
+++ src/Base/Weapon.java
@@ -7,29 +7,27 @@ public class Weapon extends Sprite{
 
     private WeaponStats stats;
 
-    public Weapon(BufferedImage image, WeaponStats setStats)
-    {
+    public Weapon(BufferedImage image, WeaponStats setStats) {
         super(0, 0, 0, image);
         loadStats(setStats);
     }
 
-    public Weapon(int x, int y, BufferedImage image, WeaponStats setStats)
-    {
+    public Weapon(int x, int y, BufferedImage image, WeaponStats setStats) {
         super(x, y, 0, image);
         loadStats(setStats);
     }
 
-    public void loadStats(WeaponStats setStats)
-    {
+    public void loadStats(WeaponStats setStats) {
         this.stats = setStats;
     }
 
-    public int use()
-    {
+    public int use() {
         stats.durability -= 1;
         return stats.damage;
     }
 
-    public boolean noDurability() {return stats.durability < 1;}
+    public boolean noDurability() {
+        return stats.durability < 1;
+    }
 
 }
